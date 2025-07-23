95e0c0018371bd81721fa9116475e6d3f765881f
arturstaation <arturmackenzie12@gmail.com>
2025-07-22 21:33:29 -0300

    deixando o driver headless

--- Services/SeleniumWebDriverService.py
+++ Services/SeleniumWebDriverService.py
@@ -26,6 +26,8 @@ class SeleniumWebDriverService:
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--window-size=1920x1080")
+        options.add_argument("--headless")
+        options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36")
         proxyService = ProxyService(self.logger)
         proxyService.getProxies()
 
